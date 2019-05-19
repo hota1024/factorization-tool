3a766eaba02c46e133bbe978cd518958b60f0bf1
hota1024 <hotalog@hota1024.com>
2019-05-19 20:21:49 +0900

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,84 @@
+import VuetifyLoaderPlugin from 'vuetify-loader/lib/plugin'
+import pkg from './package'
+
+export default {
+  mode: 'universal',
+
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: '素因数分解',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: pkg.description }
+    ],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: '/factorization-page/favicon.ico'
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+      }
+    ]
+  },
+
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+
+  /*
+   ** Global CSS
+   */
+  css: ['~/assets/style/app.styl'],
+
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: ['@/plugins/vuetify'],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: ['@nuxtjs/pwa'],
+
+  /*
+   ** Router
+   */
+  router: {
+    base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/factorization/' : '/'
+  },
+
+  /*
+   ** Build configuration
+   */
+  build: {
+    transpile: ['vuetify/lib'],
+    plugins: [new VuetifyLoaderPlugin()],
+    loaders: {
+      stylus: {
+        import: ['~assets/style/variables.styl']
+      }
+    },
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {
+      // Run ESLint on save
+      if (ctx.isDev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <h1 class="my-4 display-3">素因数分解</h1>
+      <v-text-field v-model="n" label="分解する数値" />
+      <v-btn color="info" depressed block @click="factorization">分解</v-btn>
+      結果：{{ formatted }}
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+import Factorization from 'factorization'
+
+export default {
+  data() {
+    return {
+      n: 1,
+      result: []
+    }
+  },
+  computed: {
+    formatted() {
+      return this.result.join(' x ')
+    }
+  },
+  methods: {
+    factorization() {
+      this.result = Factorization(this.n)
+    }
+  }
+}
+</script>
